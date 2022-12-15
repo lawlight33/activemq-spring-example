dbc84fec4e2018c1f07373ca655fce5c8028e4aa
promark33 <promark33@gmail.com>
2022-12-15 16:50:57 +0100

    Acknowledge mode parameter

--- src/main/java/com/example/JmsConfiguration.java
+++ src/main/java/com/example/JmsConfiguration.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import com.example.services.Consumer;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.RedeliveryPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
@@ -11,6 +16,8 @@ import javax.jms.ConnectionFactory;
 
 @Configuration
 public class JmsConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
     public static final String BROKER_URL = "tcp://localhost:61616";
 
@@ -28,6 +35,26 @@ public class JmsConfiguration {
     public ConnectionFactory connectionFactory() {
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL(BROKER_URL);
+        // Custom redelivery policy with no cap for redeliveries
+        RedeliveryPolicy redeliveryPolicy = new RedeliveryPolicy();
+        redeliveryPolicy.setMaximumRedeliveries(RedeliveryPolicy.NO_MAXIMUM_REDELIVERIES);
+        activeMQConnectionFactory.setRedeliveryPolicy(redeliveryPolicy);
         return activeMQConnectionFactory;
+    }
+
+    // Ability to set error handler - if @JmsListener methods will throw exceptions
+    // But in case of exception occurred and the session working in acknowledge
+    // modes AUTO_ACKNOWLEDGE or DUPS_OK_ACKNOWLEDGE -> messages still will be marked as received
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        // Custom acknowledge mode: guaranteed redelivery in case if exception in consumer listener or JVM dye
+        // more on this: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/jms/listener/AbstractMessageListenerContainer.html
+        factory.setSessionTransacted(true);
+        // Other acknowledge modes can be set via: factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+        factory.setErrorHandler(ex -> logger.error("JMS exception occurred!!!", ex));
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 }
