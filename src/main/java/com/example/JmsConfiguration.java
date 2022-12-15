6d1a8c23a2b27dd50759e0f1b89515d3573e0e5d
promark33 <promark33@gmail.com>
2022-12-15 18:29:07 +0100

    Multiply consumers

--- src/main/java/com/example/JmsConfiguration.java
+++ src/main/java/com/example/JmsConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 import javax.jms.ConnectionFactory;
+import java.util.UUID;
 
 @Configuration
 public class JmsConfiguration {
@@ -32,7 +33,7 @@ public class JmsConfiguration {
     }
 
     @Bean
-    public ConnectionFactory connectionFactory() {
+    public ConnectionFactory connectionFactory1() {
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL(BROKER_URL);
         // Custom redelivery policy with no cap for redeliveries
@@ -42,21 +43,31 @@ public class JmsConfiguration {
         return activeMQConnectionFactory;
     }
 
-    // Ability to set error handler - if @JmsListener methods will throw exceptions
-    // But in case of exception occurred and the session working in acknowledge
-    // modes AUTO_ACKNOWLEDGE or DUPS_OK_ACKNOWLEDGE -> messages still will be marked as received
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory1(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        return createJmsContainerFactory(UUID.randomUUID().toString(), connectionFactory, messageConverter);
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory2(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        return createJmsContainerFactory(UUID.randomUUID().toString(), connectionFactory, messageConverter);
+    }
+
+    private DefaultJmsListenerContainerFactory createJmsContainerFactory(String clientId, ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         // Custom acknowledge mode: guaranteed redelivery in case if exception in consumer listener or JVM dye
         // more on this: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/jms/listener/AbstractMessageListenerContainer.html
-        factory.setSessionTransacted(true);
         // Other acknowledge modes can be set via: factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+        factory.setSessionTransacted(true);
+        // Setting error handler - if @JmsListener methods will throw exceptions
+        // But in case of exception occurred and the session working in acknowledge
+        // modes AUTO_ACKNOWLEDGE or DUPS_OK_ACKNOWLEDGE -> messages still will be marked as received
         factory.setErrorHandler(ex -> logger.error("JMS exception occurred!!!", ex));
         factory.setMessageConverter(messageConverter);
         // Set "true" for Topic instead of Queue
         factory.setPubSubDomain(false);
+        factory.setClientId(clientId);
         return factory;
     }
 }
--- src/main/java/com/example/services/ConsumerGroup.java
+++ src/main/java/com/example/services/ConsumerGroup.java
@@ -7,17 +7,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * Creates one {@link JmsListener} per each DefaultJmsListenerContainerFactory declared in {@link JmsConfiguration}.
+ * Each {@link JmsListener} comes with unique client id.
+ */
 @Component
 public class ConsumerGroup {
 
     private static final Logger logger = LoggerFactory.getLogger(ConsumerGroup.class);
 
-    @JmsListener(destination = JmsConfiguration.DESTINATION_NAME)
+    @JmsListener(destination = JmsConfiguration.DESTINATION_NAME, containerFactory = "jmsListenerContainerFactory1")
     public void consumer1(Order order) {
         logger.info("[Consumer1] Received message: {}", order);
     }
 
-    @JmsListener(destination = JmsConfiguration.DESTINATION_NAME)
+    @JmsListener(destination = JmsConfiguration.DESTINATION_NAME, containerFactory = "jmsListenerContainerFactory2")
     public void consumer2(Order order) {
         logger.info("[Consumer2] Received message: {}", order);
     }
